d583873acf092df6b14499df7140d5d162e25d57
surendra3478 <30794351+surendra3478@users.noreply.github.com>
2017-08-09 15:34:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.send(`article requested will be served here`);
+ res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/article-two', function (req, res) {
   res.send(`article requested will be served here`);
