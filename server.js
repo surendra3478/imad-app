8c0d8dc93971a67f598d6eae0f50c1c33427ee82
surendra3478 <30794351+surendra3478@users.noreply.github.com>
2017-08-09 17:22:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -42,41 +42,7 @@ var articles= {
     
     }
 };
-function createTemplate(data)
-{
-    var title=data.title;
-    var heading=data.heading;
-    var date=data.date;
-    var content=data.content;
-var htmlTemplate=`
-<html>
-    <head>
-         <title>${title};</title>
-          <link href="/ui/style.css" rel="stylesheet" />
-    </head>
-    <body>
-        <div class="container">
-                
-                 
-                <div>
-                    <a href="/">home</a>
-                </div>
-                <hr>
-                <div>
-                    ${heading};
-                </div>
-                <div>
-                   ${date}
-                </div>
-                <div>
-                   ${content}
-                    
-                </div>
-    </div>
-    </body>
-</html>`;
-return htmlTemplate;
-}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
