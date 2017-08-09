91d33d3a386056d0ce8b2aa0dfa0c7e8cd5965f3
surendra3478 <30794351+surendra3478@users.noreply.github.com>
2017-08-09 17:18:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,8 +89,6 @@ app.get(':articleName', function (req, res) {
     var articleName=req.params.articleName;
  res.send(createTemplate(articles[articleName]));
 });
- 
- 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
