30935eee06444ebc1656d4cf525b9366d6d75337
surendra3478 <30794351+surendra3478@users.noreply.github.com>
2017-08-09 15:21:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,20 +13,14 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/article-one.html', function (req, res) {
-    //res.sendFile(path.join(__direname,`úi`,`árticle-one.html`));
-    res.send(`article-one requested and will be served here`);
-    
+app.get('/article.html', function (req, res) {
+  res.send(`article requested will be served here`);
 });
-app.get(`ui/article-two.html`,function(req,res){
-    //res.sendFile(path.join(__direname,`úi`,`árticle-two.html`));
-    res.send(`article-one requested and will be served here`);
-    
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get(`ui/article-three.html`,function(req,res){
-    //res.sendFile(path.join(__direname,`úi`,`árticle-one.html`));
-    res.send(`article-one requested and will be served here`);
-    
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
