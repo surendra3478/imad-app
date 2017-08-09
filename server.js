c0ff64937a155add2f0ec64f1f8eb936e81e8de9
surendra3478 <30794351+surendra3478@users.noreply.github.com>
2017-08-09 15:23:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,12 +16,14 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/article-one', function (req, res) {
   res.send(`article requested will be served here`);
 });
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+app.get('/article-two', function (req, res) {
+  res.send(`article requested will be served here`);
 });
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+app.get('/article-three', function (req, res) {
+  res.send(`article requested will be served here`);
 });
+
+ 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
