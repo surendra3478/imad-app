ff9d4b96a9c2ed9228a7e21b30f889c6f8bb4f22
surendra3478 <30794351+surendra3478@users.noreply.github.com>
2017-08-11 10:50:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,15 @@ element.innerHTML="new text";
 
 
 var img=document.getElementById("modi");
+
+
+var marginLeft=0;
+function moveRight()
+{
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=maxlength+'px';
+}
 img.onclick=function(){
-    img.style.marginLeft='100px';
+    var Interval=setInterval(moveRight,50);
+    
 }
