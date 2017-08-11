4170135ad33c192965de68b9c583e86671f3fcde
surendra3478 <30794351+surendra3478@users.noreply.github.com>
2017-08-11 15:45:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,7 +91,7 @@ app.get('/ui/style.css', function (req, res) {
 });
 var names=[];
 app.get('/submit-btn',function (req,res){
-    alert(req.query.name);
+    
     var name=req.query.name;
     names.push(name);
     res.send(JSON.stringify(names));
