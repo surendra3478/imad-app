979ed26d60d7af09fe86f41886b68167d92d35f0
surendra3478 <30794351+surendra3478@users.noreply.github.com>
2017-08-11 10:52:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -10,7 +10,7 @@ var marginLeft=0;
 function moveRight()
 {
     marginLeft=marginLeft+10;
-    img.style.marginLeft=maxlength+'px';
+    img.style.marginLeft=marginLeft+'px';
 }
 img.onclick=function(){
     var Interval=setInterval(moveRight,50);
