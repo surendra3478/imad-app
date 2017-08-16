3342bcf7f357e2b317e0c1bce04469e4753adb5c
surendra3478 <30794351+surendra3478@users.noreply.github.com>
2017-08-16 12:00:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -115,7 +115,7 @@ app.get('/test-db', function (req, res) {
      }
      else
      {
-         res.send(JSON.stringify(result));
+         res.send(JSON.stringify(result.rows));
      }
  });
     
