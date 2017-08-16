26615584270ce3bee89284fb6634923973a7968d
surendra3478 <30794351+surendra3478@users.noreply.github.com>
2017-08-16 12:18:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -141,7 +141,7 @@ app.get('/articles/:articleName', function (req, res) {
              }
              else
              {
-             var articleData=res.rows[0];
+             var articleData=result.rows[0];
              res.send(createTemplate(articleData));
              }
          }
