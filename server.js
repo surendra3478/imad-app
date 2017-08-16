7d8112af17a66ada128d27ca6f5106b3fddc2571
surendra3478 <30794351+surendra3478@users.noreply.github.com>
2017-08-16 12:12:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -125,7 +125,7 @@ app.get('/test-db', function (req, res) {
 app.get('/articles/articleName', function (req, res) {
     
     var articleName=req.params.articleName;
-    pool.query("select * from articles where title='"+ articleName+"'",function(err,result){
+    pool.query("select * from article where title='"+ articleName+"'",function(err,result){
          if(err)
          {
              res.status(500).send(err.toString());
