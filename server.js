673bc725e6905f4d8a6dea1b4c30399a4a338a9d
surendra3478 <30794351+surendra3478@users.noreply.github.com>
2017-08-18 12:39:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool=require('pg').Pool;
+var crypto=require('crypto');
 var config={
     user:'surendrakakinada',
     database:'surendrakakinada',
@@ -12,7 +13,7 @@ var config={
 
 var app = express();
 app.use(morgan('combined'));
-var articles= {
+//var articles= {
    'article-one' : {
     title:`Article one heading`,
     heading:`Article one`,
@@ -157,7 +158,14 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-
+function hash(input,salt){
+    var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
+    return hashed.toString('hex');
+}
+app.get('/hash/:input', function (req, res) {
+  var hasedString=hash(req.params.input,'this-is-some-random-string');
+  res.send(hasedString);
+});
 
 
 // Do not change port, otherwise your app won't run on IMAD servers
