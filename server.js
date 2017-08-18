ef81cad582752d719620e8b437e4ea4f657ff366
surendra3478 <30794351+surendra3478@users.noreply.github.com>
2017-08-18 12:58:39 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -158,10 +158,10 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-function hash(input,salt){
+/*function hash(input,salt){
     var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
     return hashed.toString('hex');
-}
+}*/
 app.get('/hash/:input', function (req, res) {
     res.send( "select * from article where title='"+ articleName+"'");
   var hashedString=hash(req.params.input,'this-is-some-random-string');
