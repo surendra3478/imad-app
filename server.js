4e9202ad723fd16e448a3c3a130e5da30bfc778f
surendra3478 <30794351+surendra3478@users.noreply.github.com>
2017-08-19 11:05:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -158,15 +158,19 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-function hash1(input,salt){
+function hash(input,salt){
     var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
     return ["pbkdf2","10000",salt,hashed.toString('hex')].join("$");
 }
 app.get('/hash/:input', function (req, res) {
-  var hashedString=hash1(req.params.input,'This-is-some-random-string');
+  var hashedString=hash(req.params.input,'This-is-some-random-string');
   res.send(hashedString);
 });
-
+app.get('/create-user', function (req, res) {
+ var salt=crypto.getRandomBytes(128).toString("hex");
+ var dbString=hash(password,salt);
+  res.send(hashedString);
+});
 
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
