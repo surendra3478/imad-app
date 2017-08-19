4a18d1bb90b31ecaf355975dc1dd0280e3221ae8
surendra3478 <30794351+surendra3478@users.noreply.github.com>
2017-08-19 15:43:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -173,17 +173,9 @@ app.post('/create-user', function (req, res) {
  var password=req.body.password;
  var salt=crypto.randomBytes(128).toString("hex");
  var dbString=hash(password,salt);
- pool.query('insert into "user" values($1,$2)',[username,dbString],function(err,result){
-      res.send("user");
-     if(err)
-     {
-         
-         res.status(500).send(err.toString());
-     }
-     else
-     {
+ 
          res.send("user Created successfully",username);
-     }
+     
  });
    
 });
