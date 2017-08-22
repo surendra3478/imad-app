6eccf99ebc05bae554a6471cfbd97c85adb575c5
surendra3478 <30794351+surendra3478@users.noreply.github.com>
2017-08-22 22:02:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -162,7 +162,7 @@ app.get('/ui/main.js', function (req, res) {
 
 function hash1(input,salt){
     var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
-   // return ["pbkdf2","10000",salt,hashed.toString('hex')].join("$");
+   return ["pbkdf2","10000",salt,hashed.toString('hex')].join("$");
 }
 app.get('/hash/:input', function (req, res) {
   var hashedString=hash1(req.params.input,'This-is-some-random-string');
@@ -170,7 +170,7 @@ app.get('/hash/:input', function (req, res) {
 });
 app.post('/create-user', function (req, res) {
  var username=req.body.username;
- var password=req.body.password;
+ var password='hex';
  //var salt=crypto.randomBytes(128).toString('hex');
  var salt='hex';
  var dbString=hash1(password,salt);
