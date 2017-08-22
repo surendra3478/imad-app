64ebecc9361751d6cac8281df3422dc218968c56
surendra3478 <30794351+surendra3478@users.noreply.github.com>
2017-08-22 22:33:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -66,6 +66,7 @@ submit.onclick=function(){
      console.log(username);
      console.log(password);
      alert(username);
+     alert(password);
     request.open('POST', 'http://surendrakakinada.imad.hasura-app.io/login', true);
     request.setRequestHeader('Content-Type','application/json');
     request.send(JSON.stringify({username:username, password:password}));
