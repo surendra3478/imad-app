bb1f895c0fad75d9b1481a3e07221285d6ed3368
surendra3478 <30794351+surendra3478@users.noreply.github.com>
2017-08-22 22:01:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -160,12 +160,12 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-function hash(input,salt){
+function hash1(input,salt){
     var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
     return ["pbkdf2","10000",salt,hashed.toString('hex')].join("$");
 }
 app.get('/hash/:input', function (req, res) {
-  var hashedString=hash(req.params.input,'This-is-some-random-string');
+  var hashedString=hash1(req.params.input,'This-is-some-random-string');
   res.send(hashedString);
 });
 app.post('/create-user', function (req, res) {
@@ -173,7 +173,7 @@ app.post('/create-user', function (req, res) {
  var password=req.body.password;
  //var salt=crypto.randomBytes(128).toString('hex');
  var salt='hex';
- var dbString=hash(password,salt);
+ var dbString=hash1(password,salt);
  pool.query('insert into "user" values($1,$2)',[username,dbString],function(err,result){
       //res.send("user");
      if(err)
