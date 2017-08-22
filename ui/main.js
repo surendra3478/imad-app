75ba32c258f4a7da586e11abec20a3f8c6e639c5
surendra3478 <30794351+surendra3478@users.noreply.github.com>
2017-08-22 23:10:17 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,37 +1,8 @@
 console.log('Loaded!');
 
 
-/* var img=document.getElementById("modi");
 
 
-var marginLeft=0;
-function moveRight()
-{
-    marginLeft=marginLeft+10;
-    img.style.marginLeft=marginLeft+'px';
-}
-img.onclick=function(){
-    var Interval=setInterval(moveRight,50);
-    
-}*/
-var btn=document.getElementById("counter");
- 
-btn.onclick=function(){
-    var request=new XMLHttpRequest();
-    request.onreadystatechange = function(){
-        if (request.readyState === XMLHttpRequest.DONE) {
-        // Everything is good, the response was received.
-          if(request.status==200)
-          {
-            var counter=request.responseText;
-            var span=document.getElementById("count");
-            span.innerHTML=counter.toString();
-          }
-        }  
-    };
-    request.open('GET', 'http://surendrakakinada.imad.hasura-app.io/counter', true);
-    request.send(null);
-};
 
 //submit
 
