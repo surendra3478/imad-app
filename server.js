88a1df0fa7e11bc1359f83f7c2cab84cb20e3d67
surendra3478 <30794351+surendra3478@users.noreply.github.com>
2017-08-23 12:35:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -196,6 +196,7 @@ app.post('/create-user', function (req, res) {
 app.post('/login', function (req, res) {
  var username=req.body.username;
  var password=req.body.password;
+  res.send('select * from "user" where username= $1',[username]);
  pool.query('select * from "user" where username= $1',[username],function(err,result){
       if (err)
     {
