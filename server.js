0bc92b227c2eab85c324f4e6bcb1d73905695f64
surendra3478 <30794351+surendra3478@users.noreply.github.com>
2017-08-27 20:46:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -172,6 +172,7 @@ app.get('/hash/:input', function (req, res) {
   res.send(hashedString);
 });
 app.post('/create-user', function (req, res) {
+    var pool = new Pool(config);
  var username=req.body.username;
  var password=req.body.password;
  //var salt=crypto.randomBytes(128).toString('hex');
