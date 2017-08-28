00d2c00b7c744feb0054e97b5a688957bf14afba
surendra3478 <30794351+surendra3478@users.noreply.github.com>
2017-08-28 13:06:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -275,22 +275,30 @@ app.post('/login',function(req,res){
  
 });
 
-app.get('/check-login',function(req,res){
-    if(req.session && req.session.auth && req.session.auth.userId )
-    {
-        res.send('you are logged in ' +  req.session.auth.userId.toString());
-    }
-    else
-    {
-        res.send('you are logged not in login again ' );
-    }
-    
-});
-
-// Do not change port, otherwise your app won't run on IMAD servers
-// Use 8080 only for local development if you already have apache running on 80
-
 var port = 80;
 app.listen(port, function () {
   console.log(`IMAD course app listening on port ${port}!`);
 });
+
+
+app.get('/check-login',function(req,res){
+    if( req.session && req.session.auth && req.session.auth.userId)
+    {
+        res.send("you are logged in " +  req.session.auth.userId.toString());
+    }
+    else
+    {
+        res.send("you are logged not in login again");
+    }
+     
+    
+});
+app.get('/logout',function(req,res){
+    delete req.session.auth;
+     res.send("you are loggedout login again");
+});
+
+
+// Do not change port, otherwise your app won't run on IMAD servers
+// Use 8080 only for local development if you already have apache running on 80
+
