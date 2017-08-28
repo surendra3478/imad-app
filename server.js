c963d0b84277f24e363b5b83375d5ac8a391dcf9
surendra3478 <30794351+surendra3478@users.noreply.github.com>
2017-08-28 13:08:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -281,7 +281,7 @@ app.listen(port, function () {
 });
 
 
-app.get('/check-login',function(req,res){
+app.get('/check-login',function (req,res){
     if( req.session && req.session.auth && req.session.auth.userId)
     {
         res.send("you are logged in " +  req.session.auth.userId.toString());
